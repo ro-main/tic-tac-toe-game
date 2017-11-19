d5dc6d0aad82d0d0ce5919e86189eb5b4d8f84b9
Romain Bouic <romainbouic@gmail.com>
2017-11-19 22:57:39 +0100

    remove comments

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+/* eslint no-use-before-define: ["error", { "functions": false }] */
 
 // list all possible ways to win
 const winCombinations = [
@@ -16,77 +17,51 @@ const winCombinations = [
 const player1 = { number: 1, played: [], possibleWins: winCombinations, color: 'player-1', wins: 0 };
 const player2 = { number: 2, played: [], possibleWins: winCombinations, color: 'player-2', wins: 0 };
 
+let playerStartingGame = 1;
+
+const availableBlocks = document.querySelectorAll('div.available');
+
 function updateOtherPlayerPossibleWins(OtherPlayer, lastMove) {
   console.log('FUNCTION UPDATE_OTHER_PLAYER_POSSIBLE_WINS()');
   const currMove = Number(lastMove);
-  let stringifiedPossibleWins = '';
   const newPossibleWins = [];
-  let stringifiedNewPossibleWins = '';
-  let i;
-  // for loop is only used for console log
-  for (i = 0; i < OtherPlayer.possibleWins.length; i += 1) {
-    stringifiedPossibleWins += `[${OtherPlayer.possibleWins[i]}] `;
-  }
-  console.log(`Other player is ${OtherPlayer.number} and his possible wins (before this move) are: ${stringifiedPossibleWins}`);
-  console.log(`Last move is block ${currMove}`);
-  console.log('Checking in each possible win if last move is present');
+
   OtherPlayer.possibleWins.forEach((e) => {
-    console.log(`"e: "${e}, lastmove: ${currMove}`);
     if (!e.includes(currMove)) {
-      console.log(`Checking if last move, ${currMove}, is in [${e}] => No, we keep it.`);
       newPossibleWins.push(e);
-    } else {
-      console.log(`Checking if last move, ${currMove}, is in [${e}] => Yes, we discard it.`);
     }
   });
-  let f;
-  for (f = 0; f < newPossibleWins.length; f += 1) {
-    stringifiedNewPossibleWins += `[${newPossibleWins[f]}]`;
-  }
-  console.log(`Updated list of the other player possible wins: ${stringifiedNewPossibleWins}`);
   return newPossibleWins;
 }
 
 function winCheck(currentPlayer) {
-  console.log('FUNCTION WIN_CHECK()');
   const joueur = currentPlayer;
   let yes;
-  console.log(`Checking if the blocks played ${joueur.played} can match a win combination within the ${joueur.possibleWins.length} possible wins`);
-  // we are going to check each possible wins until we find one matching the played ones
   let k;
   for (k = 0; k < joueur.possibleWins.length; k += 1) {
-    console.log(`Checking possible wins option number ${k + 1}/${joueur.possibleWins.length}: ${joueur.possibleWins[k]}`);
-    // we check if we find each
     yes = 0;
     let l;
     for (l = 0; l < joueur.played.length; l += 1) {
-      console.log(`Checking if ${joueur.played[l]} (played in ${l + 1}/${joueur.played.length} position), is in ${joueur.possibleWins[k]}`);
-      console.log(joueur.possibleWins[k].indexOf(joueur.played[Number(l)]));
-      if (joueur.possibleWins[k].indexOf(joueur.played[Number(l)]) !== -1) {
+      const playerCurrPossibleWin = joueur.possibleWins[k];
+      const playerCurrMove = joueur.played[Number(l)];
+      const playerCurrMovePos = playerCurrPossibleWin.indexOf(playerCurrMove);
+      if (playerCurrMovePos !== -1) {
         yes += 1;
-        console.log(`Yes it is! We have ${yes} match in ${joueur.possibleWins[k]}`);
-      } else {
-        console.log('Nope it is not in it');
       }
 
       if (yes === 3) {
-        console.log('We have a winner!');
         return 'winner';
       }
-      console.log('End of loop on played for the current possible wins');
     }
   }
   console.log('End of loop on possible wins. No winner');
-  return 'No winner yet. Let\'s keep play';
+  return 'No winner yet';
 }
 
 function updateVisual(turn, block, currentPlayer, nextTurn) {
   const currBlock = block;
-  console.log('FUNCTION UPDATEVISUAL()');
   currBlock.className = `block , ${currentPlayer.color}`;
-  console.log(`Adding the current PLayer's color (number ${currentPlayer.number}) to block ${block.id}`);
-  document.getElementById('turn').innerHTML = `Player ${nextTurn}`;
-  console.log(`Modifying the turn in the interface from ${currentPlayer.number} to ${nextTurn}`);
+  document.getElementById('turn-value').innerHTML = `Player ${nextTurn}`;
 }
 
 function updateData(turn, blockId, currentPlayer, nextPlayer) {
@@ -111,30 +86,72 @@ function updateData(turn, blockId, currentPlayer, nextPlayer) {
   console.log(`Back in FUNCTION UPDATEDATA() - Next player updated possiblewins are: ${stringifiedNextPlayerPossibleWins}`);
 }
 
-function reset() {
-  console.log('FUNCTION RESET()');
-  const blocks = document.querySelectorAll('div.block');
-  blocks.forEach((block) => {
+function cleanCurrGame() {
+  console.log('FUNCTION CLEANCURRENTGAME()');
+  const playedBlocks = document.querySelectorAll('div.block');
+  playedBlocks.forEach((block) => {
     const currBlock = block;
     currBlock.className = 'block available';
   });
-  console.log('setting blocks class to block available');
   player1.possibleWins = winCombinations;
   player2.possibleWins = winCombinations;
-  console.log('reset players possible wins');
   player1.played = [];
   player2.played = [];
-  console.log('reset players played moves');
   document.getElementById('end').style.display = 'none';
   document.getElementById('board').style.display = 'block';
-  document.getElementById('turn').innerHTML = 'Player 1';
-  console.log('hide the banner and displayer the board');
-  console.log('show that it\'s player 1 turn');
 }
 
-function end(status, currentPlayer) {
+function cleanCurrSession() {
+  console.log('FUNCTION CLEANCURRENTSESSION()');
+  player1.wins = 0;
+  player2.wins = 0;
+  document.getElementById('player1-wins').innerHTML = '0';
+  document.getElementById('player2-wins').innerHTML = '0';
+}
+
+function reset() {
+  console.log('FUNCTION RESET()');
+  cleanCurrGame();
+  cleanCurrSession();
+  document.getElementById('reset').innerHTML = 'Reset';
+  document.getElementById('turn-wrapper').style.display = 'block';
+  playerStartingGame = Math.floor((Math.random() * 2) + 1);
+  if (playerStartingGame === 1) {
+    document.getElementById('turn-value').innerHTML = 'Player 1';
+  } else {
+    document.getElementById('turn-value').innerHTML = 'Player 2';
+  }
+  console.log(`Selecting randomly the player who will start: ${playerStartingGame}. Starting the game.`);
+  play(playerStartingGame);
+}
+
+function nextGame() {
+  console.log('FUNCTION NEXTGAME()');
+  console.log(`previous playerStartingGame is ${playerStartingGame}`);
+  console.log('Calling function cleanCurrGame()');
+  cleanCurrGame();
+  console.log('Back in function nextGame()');
+
+  if (playerStartingGame === 1) {
+    document.getElementById('turn-value').innerHTML = 'Player 2';
+    playerStartingGame = 2;
+    console.log(`playerStartingGame is ${playerStartingGame} (should be 2)`);
+    play(playerStartingGame);
+  } else {
+    document.getElementById('turn-value').innerHTML = 'Player 1';
+    playerStartingGame = 1;
+    console.log(`playerStartingGame is ${playerStartingGame} (should be 1)`);
+    play(playerStartingGame);
+  }
+}
+
+function endGame(status, currentPlayer) {
+  console.log('FUNCTION ENDGAME()');
+  availableBlocks.forEach((block) => {
+    block.removeEventListener('click', () => {}, { once: true });
+  });
   const currPlayer = currentPlayer;
-  if (status) {
+  if (status === 'winner') {
     document.getElementById('end').innerHTML = `Player ${currentPlayer.number} wins!`;
     currPlayer.wins += 1;
     document.getElementById(`player${currentPlayer.number}-wins`).innerHTML = currentPlayer.wins;
@@ -143,20 +160,24 @@ function end(status, currentPlayer) {
   }
   document.getElementById('end').style.display = 'block';
   document.getElementById('board').style.display = 'none';
+  console.log('Setting timeout & calling function Nextgame()');
   setTimeout(() => {
-    reset();
+    nextGame();
   }, 3000);
 }
 
 function play(turn) {
+  let currTurn;
+  if (player1.played.length === 0 && player2.played.length === 0) {
+    currTurn = turn;
+  }
   let nextTurn;
   let currentPlayer = {};
   let nextPlayer = {};
-  const blocks = document.querySelectorAll('div.available');
-  console.log(`FUNCTION PLAY() is called for Player ${turn}`);
-  blocks.forEach((block) => {
+  console.log(`FUNCTION PLAY() is called for Player ${currTurn}`);
+  availableBlocks.forEach((block) => {
     block.addEventListener('click', () => {
-      if (turn === 1) {
+      if (currTurn === 1) {
         currentPlayer = player1;
         nextPlayer = player2;
         nextTurn = 2;
@@ -168,28 +189,33 @@ function play(turn) {
         console.log(`Current player is ${currentPlayer.number} (should be 2) - Next player is ${nextPlayer.number} (should be 1)`);
       }
       console.log(`Block ${block.id} has been clicked on`);
-      updateVisual(turn, block, currentPlayer, nextTurn);
-      updateData(turn, Number(block.id), currentPlayer, nextPlayer);
+      updateVisual(currTurn, block, currentPlayer, nextTurn);
+      updateData(currTurn, Number(block.id), currentPlayer, nextPlayer);
 
-      let status = 'no winner yet';
+      let status = 'No winner yet';
       console.log(`Back in FUNCTION PLAY() - Current player has played ${currentPlayer.played.length} times.`);
-      if (currentPlayer.played.length >= 3 && currentPlayer.played.length <= 5) {
-        console.log('It is between 3 and 5 => we are going to check if there is a win.');
+      if (currentPlayer.played.length >= 3) {
+        console.log('It is >= 3, we are going to check if there is a win.');
         status = winCheck(currentPlayer);
         if (status === 'winner') {
-          end(status, currentPlayer);
+          console.log(`Status is "${status}" (should be winner)`);
+          endGame(status, currentPlayer);
+        } else if (status !== 'winner' && currentPlayer.played.length === 5) {
+          console.log(`Status is "${status}" (should be "No winner yet") and player played ${currentPlayer.played.length} times (should be 5) => It's a draw, we are ending the game.`);
+          endGame(status, currentPlayer);
         } else {
-          turn = nextTurn;
+          console.log(`Status is "${status}" (should be "No winner yet") and player played
+          ${currentPlayer.played.length} times (should not be 5). Player ${currTurn} just played, now it's player ${nextTurn}'s turn`);
+          currTurn = nextTurn;
+          console.log(`currTurn is now: ${currTurn}`);
         }
-      } else if (currentPlayer.played.length === 5) {
-        console.log('It is equal to 5 => We are ending the game.');
-        end(status, currentPlayer);
       } else {
         console.log('It is less than 3 => we are moving to the next player without checking if there is a win.');
-        turn = nextTurn;
+        currTurn = nextTurn;
         console.log('=====================================');
-        console.log(`IT IS NOW PLAYER'S ${turn} TURN.`);
+        console.log(`IT IS NOW PLAYER'S ${currTurn} TURN.`);
       }
+      console.log(`Waiting for next click by ${currTurn}`);
       // console.info(playedBy1, playedBy2);
     },
     { once: true },
@@ -199,5 +225,4 @@ function play(turn) {
 
 document.getElementById('reset').addEventListener('click', () => {
   reset();
-  play(1);
 });
